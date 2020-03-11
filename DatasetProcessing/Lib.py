bd670cac6045d5c26baab9db1f38343f58ba58d1
Siddhartha Shankar Das <siddhartha047@gmail.com>
2020-03-10 23:19:29 -0400

    newsgroup added

---
+++ DatasetProcessing/Lib.py
@@ -0,0 +1,261 @@
+import gzip
+import pickle
+import re
+
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from scipy import io
+
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+lem = WordNetLemmatizer()
+stem = PorterStemmer()
+
+stop_words = stopwords.words('english')
+
+def processText(text):
+    print("-----------")
+    print(text)
+    print("-----------")
+    # Remove punctuations
+    text = re.sub('[^a-zA-Z]', ' ', text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # remove tags
+    text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
+
+    #remove special characters and digits
+    text = re.sub("(\\d|\\W)+", " ", text)
+
+    ##Convert to list from string
+    text = text.split()
+
+    ##Stemming
+    ps = PorterStemmer()
+
+    # Lemmatisation
+    lem = WordNetLemmatizer()
+    text = [lem.lemmatize(word) for word in text if not word in stop_words]
+    text = [word for word in text if len(word)>2]
+    # text = " ".join(text)
+    return text
+
+def processTextParagraph(text):
+    return " ".join(processText(text))
+
+def num(s):
+    try:
+        return float(s)
+    except ValueError:
+        return 0
+
+def parse(path):
+  g = gzip.open(path, 'r')
+  for l in g:
+    yield eval(l)
+
+
+#mtx format
+def save_data(data_vector,data_rating,output_file,output_label,output_data,comment=""):
+    print("Writing vector as mtx file")
+    io.mmwrite(output_file, data_vector, comment=comment)
+
+    f = open(output_label, 'w')
+    f.write("%d\n" % len(data_rating))
+
+    print("Writing Class label")
+    with open(output_label, 'a') as f:
+        for item in data_rating:
+            f.write("%s\n" % item)
+    f.close()
+def save_data2(data,data_vector,data_rating,output_file,output_label,output_data,comment=""):
+    print("Started Writing data")
+    pickle.dump(data, open(output_data, "wb"))
+
+    print("Writing vector as mtx file")
+    io.mmwrite(output_file, data_vector, comment=comment)
+
+    f = open(output_label, 'w')
+    f.write("%d\n" % len(data_rating))
+
+    print("Writing Class label")
+    with open(output_label, 'a') as f:
+        for item in data_rating:
+            f.write("%s\n" % item)
+    f.close()
+
+
+def save_data_mat(home_dir,data_vector,data_rating):
+    import scipy as sp
+    #sp.io.savemat(home_dir+"data_mat.mat",mdict={'data': data})
+    sp.io.savemat(home_dir + "data_vector_mat.mat", mdict={'data_vector': data_vector})
+    sp.io.savemat(home_dir + "data_rating_mat.mat", mdict={'data_rating': data_rating})
+
+def save_data_mat2(home_dir,data,data_vector,data_rating):
+    import scipy as sp
+    sp.io.savemat(home_dir+"data_mat.mat",mdict={'data': data})
+    sp.io.savemat(home_dir + "data_vector_mat.mat", mdict={'data_vector': data_vector})
+    sp.io.savemat(home_dir + "data_rating_mat.mat", mdict={'data_rating': data_rating})
+
+def save_data_txt(home_dir,data_vector,data_rating):
+    #np.savetxt(home_dir + "data_txt.txt", data, delimiter='\t',fmt='%s'),
+    np.savetxt(home_dir + "data_vector_txt.txt", data_vector, delimiter='\t')
+    np.savetxt(home_dir + "data_rating_txt.txt", data_rating, delimiter='\t',fmt='%s')
+
+def save_data_txt2(home_dir,data,data_vector,data_rating):
+    np.savetxt(home_dir + "data_txt.txt", data, delimiter='\t',fmt='%s'),
+    np.savetxt(home_dir + "data_vector_txt.txt", data_vector, delimiter='\t')
+    np.savetxt(home_dir + "data_rating_txt.txt", data_rating, delimiter='\t',fmt='%s')
+
+
+def save_vector_mtx2(home_dir,dataset_name,data_vector):
+    filename=home_dir+dataset_name+"_vector.mtx"
+    m, n = data_vector.shape
+    header = np.array([[m, n, m * n]])
+
+    with open(filename, 'wb') as f:
+        np.savetxt(f, header, fmt='%d %d %d')
+
+    with open(filename, 'a+') as f:
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                f.write("%d %d %f\n" % (i, j, data_vector[i - 1, j - 1]))
+
+def save_data_rating_numpy(home_dir,data,data_rating):
+    np.save(home_dir+"data_np",data)
+    np.save(home_dir+"data_rating_np",data_rating)
+
+def save_vector_rating_numpy(home_dir,data_vector,data_rating):
+    np.save(home_dir+"data_vector_np",data_vector)
+    np.save(home_dir+"data_rating_np",data_rating)
+
+
+def save_vector_scipy(output_dir,data_vector):
+    import scipy.sparse
+    scipy.sparse.save_npz(output_dir+'data_vector.npz', data_vector)
+
+def load_data(home_dir):
+    data=np.load(home_dir+"data_np.npy")
+    data_rating = np.load(home_dir + "data_rating_np.npy")
+    data_vector = np.load(home_dir + "data_vector_np.npy")
+
+    return (data,data_rating,data_vector)
+
+def create_graph(home_dir):
+    (data,data_rating,data_vector)=load_data(home_dir)
+
+    n=data.shape[0]
+    indexes=np.random.choice(n, min(1000,n))
+    print(indexes)
+
+    print("Data count: ", data.shape)
+    print("Vector count: ", data_vector.shape)
+    print("Rating count: ", data_rating.shape)
+
+    data=data[indexes]
+    data_rating = data_rating[indexes]
+    data_vector = data_vector[indexes]
+
+    print("Data count: ", data.shape)
+    print("Vector count: ", data_vector.shape)
+    print("Rating count: ", data_rating.shape)
+
+    from sklearn.neighbors import kneighbors_graph
+    import networkx as nx
+    import scipy as sp
+
+    mode='distance'
+    metric='cosine'
+    #metric = 'euclidean'
+
+    k=5
+    sparse_graph = kneighbors_graph(data_vector, k, mode=mode, metric=metric, include_self=False)
+
+    if (mode == 'distance'):
+        #sparse_graph.data = 1.0 / (1.0 + sparse_graph.data)  # poincare
+        #sparse_graph.data = 1.0 - sparse_graph.data
+        sparse_graph.data = sparse_graph.data+1e-10
+        #sparse_graph.eliminate_zeros()
+
+    print('Saving graph ----')
+    graph = home_dir + 'graph.npz'
+    gephi = home_dir + "graph_gephi.gexf"
+
+    sp.sparse.save_npz(graph, sparse_graph)
+    print('Graph saving Done')
+
+    custom_labels=[]
+    for i in range(data_rating.shape[0]):
+        custom_labels.append(str(data_rating[i])+"->"+data[i][:50])
+
+    print(custom_labels)
+
+    threshold=3.0
+    if("Imdb" in home_dir): threshold=5.0
+
+    for i in range(data_rating.shape[0]):
+        if(data_rating[i]>threshold):
+            data_rating[i]=1
+        else:
+            data_rating[i] = 0
+
+
+    labels = dict(zip(range(data_rating.shape[0]), data_rating))
+    G = nx.from_scipy_sparse_matrix(sparse_graph)
+    nx.set_node_attributes(G, labels, 'labels')
+    print("Writing gephi")
+    nx.write_gexf(G, gephi)
+
+    return
+
+
+def load_model(model_name):
+    from gensim.models.doc2vec import Doc2Vec
+    import numpy as np
+
+    model = Doc2Vec.load(model_name)
+    vectors = np.zeros((model.corpus_count, model.vector_size), np.float)
+    print(vectors.shape)
+    for i in range(model.corpus_count):
+        vectors[i] = model.docvecs[i]
+    print(vectors.shape)
+
+    return vectors
+
+
+if __name__ == '__main__':
+    # home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/Imdb/aclImdb/"
+    # #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/Yelp/"
+    # #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/DBpedia/dbpedia_csv/"
+    # #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/AmazonReview/"
+    # create_graph(home_dir)
+
+    home_dir = "/Users/siddharthashankardas/Purdue/Dataset/Imdb/aclImdb/word2vec/"
+    #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/Yelp/"
+    #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/DBpedia/dbpedia_csv/"
+    #home_dir = "/Users/sid/Purdue/Research/GCSSL/Dataset/AmazonReview/"
+    #create_graph(home_dir)
+    #load_model("Models/imdb_d2v.model")
+    #data=[['This','is'],['not','meh']]
+    data=np.random.rand(5,5)
+    data_rating=np.random.rand(5,1)
+    data_vector=np.random.rand(5,10)
+    save_data_txt(home_dir, data,data_vector,data_rating)
--- DatasetProcessing/Newsgroup.py
+++ DatasetProcessing/Newsgroup.py
@@ -12,6 +12,20 @@ import timeit
 import numpy as np
 import pickle
 
+from DatasetProcessing.Lib import processText
+
+def tokenize(text):
+    min_length = 10
+
+    filtered_tokens=processText(text)
+
+    if(len(filtered_tokens)<min_length):
+        # print(text)
+        # print(filtered_tokens)
+        return ("",False)
+
+    return (filtered_tokens,True)
+
 
 def load_model(model_name):
     from DatasetProcessing.Path import pretrained_model
@@ -27,11 +41,12 @@ def load_model(model_name):
 
     return model
 
-def readData(output_dir,readall,minWordLength):
+def readData(output_dir):
     newsgroups_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
     newsgroups_test = fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'))
 
     categories=list(newsgroups_train.target_names)
+    print(categories)
 
     trsize=len(newsgroups_train.data)
     testsize=len(newsgroups_test.data)
@@ -39,73 +54,101 @@ def readData(output_dir,readall,minWordLength):
     print("Training size :",trsize)
     print("Test size :", testsize)
 
+    model=load_model("GOOGLE")
+    min_length = 10
 
-    data=[]
+    data_vector=[]
     data_rating=[]
 
     print("Started filtering text")
     index=0
     for i in range(len(newsgroups_train.data)):
-        (text,status)=tokenize(newsgroups_train.data[i])
+        (tokens,status)=tokenize(newsgroups_train.data[i])
+        print(tokens)
         if(status):
-            data.append(text)
+            vocab_tokens = [word for word in tokens if word in model.vocab]
+            if(len(vocab_tokens)<min_length):
+                print(tokens)
+                print(vocab_tokens)
+                continue
+            vector = np.mean(model[vocab_tokens], axis=0)
+            data_vector.append(vector)
             data_rating.append(newsgroups_train.target[i])
             index += 1
     print("Filtering ended")
     train_size=index
+
     for i in range(len(newsgroups_test.data)):
-        (text,status)=tokenize(newsgroups_test.data[i])
+        (tokens,status)=tokenize(newsgroups_test.data[i])
         if(status):
-            data.append(text)
+            vocab_tokens = [word for word in tokens if word in model.vocab]
+            if (len(vocab_tokens) < min_length):
+                print(tokens)
+                print(vocab_tokens)
+                continue
+            vector = np.mean(model[vocab_tokens], axis=0)
+            data_vector.append(vector)
             data_rating.append(newsgroups_test.target[i])
             index += 1
     print("Filtering ended")
-    test_size=index-train_size
 
+    print(data_vector[0])
+
+    test_size=index-train_size
     print("New train size ", train_size, " Removed :", trsize-train_size)
     print("New test size ", test_size, " Removed :",testsize-test_size)
-
     print("Total example remove: ",trsize+testsize-train_size-test_size)
-
-
-    print("Total ",len(data))
     print("Total rating ",len(data_rating))
-
-    categories_to_index= dict(zip(categories, range(len(categories))))
-    index_to_categories = np.array(categories)
-
-    print(categories_to_index)
-    print(index_to_categories)
-
-    f = open(output_dir+"categories_to_index.pkl", "wb")
-    pickle.dump(categories_to_index, f)
-    f.close()
-    np.save(output_dir + 'index_to_categories', index_to_categories)
 
     train_index = np.array(range(train_size))
     test_index = np.array(range(train_size,index))
 
-    np.save(output_dir+'train_index',train_index)
-    np.save(output_dir + 'test_index', test_index)
+    np.savetxt(output_dir+'train_index.txt',train_index)
+    np.savetxt(output_dir + 'test_index.txt', test_index)
 
-    return (np.array(data), np.array(data_rating))
+    category_map = dict()
+    print(category_map)
+
+    for category in data_rating:
+        if(category in category_map.keys()):
+            category_map[category] = category_map[category] + 1
+        else:
+            category_map[category] = 0
+
+    print(category_map)
+    with open(output_dir+"categories_all.txt","w") as f:
+        for k,v in category_map.items():
+            f.write('%s,%d\n'%(k,v))
+
+    m=len(data_rating)
+    n=300
+
+    header = np.array([[m, n, m * n]])
+
+    filename=output_dir+"newsgroup_vector.mtx"
+
+    with open(filename, 'wb') as f:
+        np.savetxt(f, header, fmt='%d %d %d')
+
+    with open(filename, 'a+') as f:
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                f.write("%d %d %f\n" % (i, j, data_vector[i - 1][j - 1]))
+
+    return (data_vector,data_rating)
 
 def read(output_dir):
     if not os.path.exists(output_dir):
         print("Creating directory: ",output_dir)
         os.makedirs(output_dir)
 
-    data_rating = []
     print("Started Reading data")
     start_reading = timeit.default_timer()
-    (data_vector, data_rating)=readData(output_dir, data_rating)
+    (data_vector, data_rating)=readData(output_dir)
     stop_reading = timeit.default_timer()
     print('Time to process: ', stop_reading - start_reading)
 
-    np.save(output_dir + "data_vector_np", data_vector)
     np.save(output_dir + "data_rating_np", data_rating)
-
-    print(data_vector.shape)
 
     return (data_vector,data_rating)
 
